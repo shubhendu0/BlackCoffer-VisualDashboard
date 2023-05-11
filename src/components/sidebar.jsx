c26d16f30700c02ff6042d4be8496d90fb05eb06
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-05-11 21:12:31 +0530

    Update sidebar.jsx

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { logout, getUser } from "../redux/features/auth/authActions";
 import { 
     Box, 
