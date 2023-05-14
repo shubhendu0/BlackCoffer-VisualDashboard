de0fe76a1f73186f7a16655c2c15bc866941f0f1
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-05-14 15:48:43 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
-//import themeReducer from "./features/theme/themeSlice";
+import themeReducer from "./features/theme/themeSlice";
 import dataReducer from "./features/data/dataSlice";
 
 
