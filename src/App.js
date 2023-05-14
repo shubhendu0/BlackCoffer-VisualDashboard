0de88df0cd127af5ea5e0763c81c01506708fb09
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-05-14 15:48:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ axios.defaults.withCredentials = true;
 function App() {
   const dispatch = useDispatch();
   const { user, isLoggedIn } = useSelector((state) => state.auth);
-  const mode = "light";
+  const mode = useSelector((state) => state.theme.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   useEffect(()=>{
