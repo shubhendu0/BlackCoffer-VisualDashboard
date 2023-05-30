620194ae78dc699d4e6cb1febfd9172dcfe569cc
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-05-31 00:49:30 +0530

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,19 @@
 import React, { useEffect } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Box, styled } from '@mui/material';
 import FirstRow from './firstRow';
 import SecondRow from './secondRow';
 import { getAllData } from '../../redux/features/data/dataActions';
 import { useDispatch } from 'react-redux';
+
+const InnerBox = styled(Box)`
+  border-radius: 10px;
+  width:98vw;
+  margin: 30px 0px;
+  padding: 0px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`; 
 
 
 const Home = () => {
@@ -11,16 +21,21 @@ const Home = () => {
     const dispatch = useDispatch();
     useEffect(()=>{
       dispatch(getAllData());
-    },[dispatch])
+    },[])
 
     return (
         <>
             <Grid container >        
                 <Grid container
                 marginTop="70px"
-                display="table-column">
-                    <FirstRow/>
-                    <SecondRow/>
+                display="table-row"
+                >
+                    <InnerBox>
+                        <FirstRow/>
+                    </InnerBox>
+                    <InnerBox>
+                        <SecondRow/>
+                    </InnerBox>
                 </Grid>
             </Grid>
         </>
