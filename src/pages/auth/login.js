7c8937da007138598f80ee99d09981da98b27b2b
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-05-31 16:56:14 +0530

    Update login.js

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -27,8 +27,8 @@ const Login=()=>{
     const boxStyle = {
       margin:"0",
       padding :"30px",
-      minWidth: "99vw",
-      minHeight:"99vh",
+      minWidth: "100vw",
+      minHeight:"100vh",
       background: "url(https://g.foolcdn.com/editorial/images/505008/getty-stock-market-chart.jpg) center/55% repeat-x #000",
       backgroundSize: "",
       backgroundRepeat: "repeat"
