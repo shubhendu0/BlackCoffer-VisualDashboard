fc217bc663eb788e379fce17d24f349ec958eec4
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-06-02 01:49:13 +0530

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,43 +1,33 @@
 import React, { useEffect } from 'react';
-import { Grid, Box, styled } from '@mui/material';
-import FirstRow from './firstRow';
-import SecondRow from './secondRow';
+import { Box, styled } from '@mui/material';
+import FirstChart from './firstChart';
+import SecondChart from './secondChart';
 import { getAllData } from '../../redux/features/data/dataActions';
 import { useDispatch } from 'react-redux';
 
 const InnerBox = styled(Box)`
   border-radius: 10px;
-  width:100vw;
-  margin: 30px 0px;
+  width:96vw;
+  margin: 60px 0px;
   padding: 0px;
   display: flex;
   align-items: center;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
 `; 
 
-
 const Home = () => {
-
     const dispatch = useDispatch();
     useEffect(()=>{
       dispatch(getAllData());
     },[])
 
     return (
-        <>
-            <Grid container >        
-                <Grid container
-                marginTop="70px"
-                display="table-row"
-                >
-                    <InnerBox>
-                        <FirstRow/>
-                    </InnerBox>
-                    <InnerBox>
-                        <SecondRow/>
-                    </InnerBox>
-                </Grid>
-            </Grid>
+        <>           
+            <InnerBox>
+                <FirstChart/>
+                <SecondChart/>
+            </InnerBox>                
         </>
     )
 }
