e9267cd1d29a75fbc5acc1d84918cda590eef89b
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-06-07 16:45:45 +0530

    Update pieChart.jsx

--- src/components/pieChart.jsx
+++ src/components/pieChart.jsx
@@ -3,11 +3,8 @@ import { styled, Box } from '@mui/material';
 import { useState, useEffect } from 'react';
 
 const Container = styled(Box)`
-    border-radius: 10px;
-    margin: auto;
-    display: flex;
+    display: block;
     align-items: center;
-    flex-direction: column;
 `;
 
 const RADIAN = Math.PI / 180;
@@ -31,7 +28,7 @@ const renderCustomizedLabel = ({
             textAnchor={x > cx ? 'start' : 'end'}
             dominantBaseline="central"
         >
-            {`${(percent * 100).toFixed(0)}%` }
+            { `${(percent * 100).toFixed(0)}%` }
         </text>
     )
 }
@@ -48,7 +45,7 @@ const CustomTooltip = ({ active, payload, label }) => {
                     border: "1px solid #cccc"
                 }}
             >
-                <label>{`${payload[0].name}`}</label>
+                <label> {`${payload[0].name}`} </label>
             </div>
         );
     }
@@ -56,7 +53,7 @@ const CustomTooltip = ({ active, payload, label }) => {
 };
 
 
-const PieChartComponent = ({data, variable, sector}) => {
+const PieChartComponent = ({data, variable, country, sector}) => {
     const [pieData, setPieData] = useState([]);
     const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#3f50b5', '#f44336', '#d500f9'];
 
@@ -64,7 +61,7 @@ const PieChartComponent = ({data, variable, sector}) => {
         const countByKey = (data, variable, sector) => {
             const counts = {};
             for (const item of data) {
-                if(item[variable] != "" && item.sector === sector){
+                if(item[variable] !== "" && item.country === country && item.sector === sector){
                     const value = item[variable];
                     counts[value] = (counts[value] || 0) + 1;
                 }             
@@ -78,18 +75,18 @@ const PieChartComponent = ({data, variable, sector}) => {
             setPieData(result);
         }         
         countByKey(data, variable, sector);
-    },[data, sector])
+    },[data, country, sector])
 
     return (
         <Container>
-            <PieChart width={450} height={500} >
+            <PieChart width={450} height={400} >
                 <Pie
                     data={pieData}
                     dataKey="count"
                     nameKey="topic"
                     cx="50%"
                     cy="50%"
-                    outerRadius={190}
+                    outerRadius={160}
                     labelLine={false}
                     label={renderCustomizedLabel}
                 >
@@ -109,7 +106,12 @@ const PieChartComponent = ({data, variable, sector}) => {
                     }                   
                 </Pie>
                 <Tooltip content={<CustomTooltip/>}/>
-                <Legend align="right" />
+                <Legend 
+                layout="horizontal" 
+                align="center" 
+                iconSize={10} 
+                verticalAlign="bottom"
+                />
             </PieChart>
         </Container>
     )
