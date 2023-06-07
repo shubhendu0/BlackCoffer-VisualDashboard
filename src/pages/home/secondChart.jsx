b0dba786f17b55a9f24e71b70828d43c7991ccf9
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-06-07 16:45:00 +0530

    Update secondChart.jsx

--- src/pages/home/secondChart.jsx
+++ src/pages/home/secondChart.jsx
@@ -4,18 +4,14 @@ import { useSelector } from 'react-redux';
 import BarChart from '../../components/barChart';
 
 const Container = styled(Box)`
-    margin: auto;
-    padding: 0px;
     display: block;
     align-items: center;
+    max-width: 600px;
 `;
 
 const InnerBox = styled(Box)`
   margin: 10px 0;
-  padding: 0px;
-  display: flex;
   align-items: center;
-  flex-direction: column;
 `; 
 
 const Heading = styled(Typography)`
@@ -25,42 +21,126 @@ const Heading = styled(Typography)`
 
 
 const SecondChart = () => {
-  const countries = ["United States of America", "Lebanon", "Nigeria",
-  "Mexico", "South Africa", "India", "Saudi Arabia", "Ukraine", "Azerbaijan",
-  "Colombia", "Niger", "Libya", "Brazil", "China", "South Sudan"
-  ]
-  
   const [country, setCountry] = useState("India");
+  const [sector, setSector] = useState("Energy");
+  const [pestle, setPestle] = useState("Industries");
+  const [source, setSource] = useState("News");
 
   const data = useSelector(state => state.data.data);
+  const uniqueValuesSet1 = new Set();
+  data.forEach(obj => {
+    const value = obj["country"];
+    if (value !== "") {
+      uniqueValuesSet1.add(value);
+    }
+  })
+  const countryArr = Array.from(uniqueValuesSet1);
+  //console.log(countryArr);
+
+  const uniqueValuesSet2 = new Set();
+  data.forEach(obj => {
+    const value = obj["sector"];
+    if (value !== "" && obj["country"]===country) {
+      uniqueValuesSet2.add(value);
+    }
+  })
+  const sectorArr = Array.from(uniqueValuesSet2);
+  //console.log(sectorArr)
+
+  const uniqueValuesSet3 = new Set();
+  data.forEach(obj => {
+    const value = obj["pestle"];
+    if (value !== "" && obj["country"]===country && obj["sector"]===sector) {
+      uniqueValuesSet3.add(value);
+    }
+  })
+  const pestleArr = Array.from(uniqueValuesSet3);
+  //console.log(topicArr)
+
+  const uniqueValuesSet4 = new Set();
+  data.forEach(obj => {
+    const value = obj["source"];
+    if (value !== "" && obj["country"]===country && obj["sector"]===sector && obj["pestle"]===pestle) {
+      uniqueValuesSet4.add(value);
+    }
+  })
+  const sourceArr = Array.from(uniqueValuesSet4);
+  //console.log(sourceArr)
+
 
   return (
     <Container>
       <InnerBox>
         <Heading> Intensity-Relevance-Likelihood </Heading>
       </InnerBox>
-
+    
       <InnerBox>
-      <FormControl sx={{ m: 0, minWidth: 150 }} size="medium">
-        <InputLabel id="select"> Countries </InputLabel>
-        <Select
-          autoWidth
-          value={country}
-          labelId="select"
-          onChange={(e)=> setCountry(e.target.value)}
-        >                  
-          <MenuItem value={0}> 
-            <em>All</em> 
-          </MenuItem>
-          {
-            countries.map((item) => (
-              <MenuItem value={item}> {item} </MenuItem>
-            ))
-          }
-        </Select>
-      </FormControl> 
-      </InnerBox>
-      <BarChart data={data} country={country}/>
+        <FormControl sx={{ m: 0, minWidth: 150 }} size="small">
+          <InputLabel id="select"> Countries </InputLabel>
+          <Select
+            autoWidth
+            value={country}
+            labelId="select"
+            onChange={(e)=> setCountry(e.target.value)}
+          >                  
+            {
+              countryArr.map((item) => (
+                <MenuItem value={item}> {item} </MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl> 
+
+        <FormControl sx={{ m: 0, minWidth: 150 }} size="small">
+          <InputLabel id="select"> Sector </InputLabel>
+          <Select
+            autoWidth
+            value={sector}
+            labelId="select"
+            onChange={(e)=> setSector(e.target.value)}
+          >                  
+            {
+              sectorArr.map((item) => (
+                <MenuItem value={item}> {item} </MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl> 
+
+        <FormControl sx={{ m: 0, minWidth: 150 }} size="small">
+          <InputLabel id="select"> Pestle </InputLabel>
+          <Select
+            autoWidth
+            value={pestle}
+            labelId="select"
+            onChange={(e)=> setPestle(e.target.value)}
+          >                  
+            {
+              pestleArr.map((item) => (
+                <MenuItem value={item}> {item} </MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl> 
+
+        <FormControl sx={{ m: 0, minWidth: 150 }} size="small">
+          <InputLabel id="select"> Source </InputLabel>
+          <Select
+            autoWidth
+            value={source}
+            labelId="select"
+            onChange={(e)=> setSource(e.target.value)}
+          >                  
+            {
+              sourceArr.map((item) => (
+                <MenuItem value={item}> {item} </MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl> 
+      
+        </InnerBox>
+      <BarChart data={data} country={country} sector={sector} pestle={pestle} source={source}/>
     </Container>
   )
 }
