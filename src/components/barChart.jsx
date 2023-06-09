8c7fa04845bb806a9958091868962a9623d5676e
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-06-09 18:49:58 +0530

    Update barChart.jsx

--- src/components/barChart.jsx
+++ src/components/barChart.jsx
@@ -5,8 +5,12 @@ import { useState, useEffect } from 'react';
 
 const Container = styled(Box)`
     display: flex;
+    justify-content: center;
     align-items: center;
-    flex-direction: column;
+    ${'' /* border: 2px solid #d1c4e9;
+    border-radius: 10px; */}
+    width: 350px;
+    height: 400px;
 `;
 
 const BarChartComponent = ({data, country, sector, pestle, source}) => {
@@ -19,11 +23,11 @@ const BarChartComponent = ({data, country, sector, pestle, source}) => {
 
     return (
         <Container>
-            <BarChart width={400} height={350} data={barData}>
+            <BarChart width={350} height={400} data={barData}>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <XAxis dataKey="topic"/>
-                {/* <YAxis/> */}
-                <Tooltip/>
+                <YAxis/>
+                <Tooltip content={<CustomTooltip />}/>
                 <Legend/>
                 <Bar dataKey="intensity" fill="#8884d8"/>
                 <Bar dataKey="relevance" fill="#2979ff"/>
@@ -32,5 +36,23 @@ const BarChartComponent = ({data, country, sector, pestle, source}) => {
         </Container>
     )
 }
+
+const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      // You can access the data values from the payload
+        const data = payload[0].payload;         
+        return (
+            <div className="custom-tooltip">
+            {/* <p className="label"> {`Topic: ${label}`} </p> */}
+            <p className="value"> {`Intensity: ${data.intensity}`} </p>
+            <p className="value"> {`Relevance: ${data.relevance}`} </p>
+            <p className="value"> {`Likelihood: ${data.likelihood}`} </p>
+            <p className="value"> {`Start-year: ${data.start_year}`} </p>
+            <p className="value"> {`End-year: ${data.end_year}`} </p>
+            </div>
+        );
+    }     
+    return null;
+};
   
 export default BarChartComponent;
